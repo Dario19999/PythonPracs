e5f818d7251f5674abede921e533d13cb68b4021
Dario Menendez <dariomego@gmail.com>
2020-05-06 20:50:17 -0500

    p13 finished

--- d08-p13-jose-menendez-W.py
+++ d08-p13-jose-menendez-W.py
@@ -3,9 +3,11 @@
 #chavez romo jonathan eduardo
 
 from colorama import init, Style, Back, Fore, Cursor
+
 init(autoreset=True)
 import os, sys, subprocess
 from msvcrt import getch
+from collections import defaultdict
 
 def printInfo():
     print(Cursor.POS(47,23)+Fore.RED+"x---------------------------x")
@@ -21,7 +23,11 @@ def menuSign():
     print(Cursor.POS(50,6)+Fore.GREEN+"| |\/| |/ _ \ '_ \| | | |")
     print(Cursor.POS(50,7)+Fore.GREEN+"| |  | |  __/ | | | |_| |")
     print(Cursor.POS(50,8)+Fore.GREEN+"\_|  |_/\___|_| |_|\__,_|")
-                         
+    
+    print(Cursor.POS(85,13)+Fore.GREEN+"[FLECHA-ARRIBA] Mover Arriba")
+    print(Cursor.POS(85,14)+Fore.GREEN+"[FLECHA-ABAJO] Mover Abajo")
+    print(Cursor.POS(85,15)+Fore.GREEN+"[ENTER] Seleccionar")                    
+    
 def newFileSign():        
     print(Cursor.POS(31,3)+Fore.RED+" _____                        ___           _     _            ")
     print(Cursor.POS(31,4)+Fore.RED+"/  __ \                      / _ \         | |   (_)           ")
@@ -31,22 +37,24 @@ def newFileSign():
     print(Cursor.POS(31,8)+Fore.RED+" \____/_|  \___|\__,_|_|    \_| |_/_|  \___|_| |_|_| \_/ \___/ ")
                                                                
 def editSign():
-    pass
-
-    
-    
+    print(Cursor.POS(47,2)+Fore.CYAN+"___  ___          _                    _______    _ _ _             ")
+    print(Cursor.POS(47,3)+Fore.CYAN+"|  \/  |         | |                  / /  ___|  | (_) |            ")
+    print(Cursor.POS(47,4)+Fore.CYAN+"| .  . | ___  ___| |_ _ __ __ _ _ __ / /| |__  __| |_| |_ __ _ _ __ ")
+    print(Cursor.POS(47,5)+Fore.CYAN+"| |\/| |/ _ \/ __| __| '__/ _` | '__/ / |  __|/ _` | | __/ _` | '__|")
+    print(Cursor.POS(47,6)+Fore.CYAN+"| |  | | (_) \__ \ |_| | | (_| | | / /  | |__| (_| | | || (_| | |   ")
+    print(Cursor.POS(47,7)+Fore.CYAN+"\_|  |_/\___/|___/\__|_|  \__,_|_|/_/   \____/\__,_|_|\__\__,_|_|   ")
     
 def menuInfo(selected = 1,fore = Fore.GREEN, back = Back.BLACK):
     if(selected == 1):
-        print(Cursor.POS(46, 13)+Fore.GREEN+Back.WHITE+"          Nuevo Archivo         ")
+        print(Cursor.POS(46, 13)+Fore.RED+Back.WHITE+"          Nuevo Archivo         ")
     else:
         print(Cursor.POS(46, 13)+fore+back+"          Nuevo Archivo         ")
     if(selected == 2):
-        print(Cursor.POS(46, 15)+Fore.GREEN+Back.WHITE+"      Mostrar/Editar Archivo    ")
+        print(Cursor.POS(46, 15)+Fore.RED+Back.WHITE+"      Mostrar/Editar Archivo    ")
     else:
         print(Cursor.POS(46, 15)+fore+back+"      Mostrar/Editar Archivo    ")
     if(selected == 3):
-        print(Cursor.POS(46, 17)+Fore.GREEN+Back.WHITE+"              Salir             ")
+        print(Cursor.POS(46, 17)+Fore.RED+Back.WHITE+"              Salir             ")
     else:
         print(Cursor.POS(46, 17)+fore+back+"              Salir             ")
     print(Cursor.POS(28,28))
@@ -73,7 +81,46 @@ def menuFrame(color = Fore.GREEN):
             
     for rightL in range(10):
         print(Cursor.POS(79,11+rightL)+color+"║")
-    
+
+def editFrame():
+    for superiorL in range(77):
+        if(superiorL == 0):
+            print(Cursor.POS(3,11)+Fore.CYAN+"╔", end="")
+        elif(superiorL == 76):
+            print(Cursor.POS(118,11)+Fore.CYAN+"╗")
+        else:
+            print(Cursor.POS(42+superiorL,11)+Fore.CYAN+"═", end="")
+            
+    for superiorL2 in range(40):   
+        if(superiorL2 == 0):
+            print(Cursor.POS(3,2)+Fore.CYAN+"╔", end="")
+        elif(superiorL2 == 39):
+            print(Cursor.POS(42,2)+Fore.CYAN+"╗")
+        else:
+            print(Cursor.POS(3+superiorL2,2)+Fore.CYAN+"═", end="") 
+             
+    for leftL in range(26):
+        print(Cursor.POS(3,3+leftL)+Fore.CYAN+"║")
+        
+    for midL in range(26):
+        if(midL == 8):
+            print(Cursor.POS(42,11)+Fore.CYAN+"╠")
+        else:
+            print(Cursor.POS(42,3+midL)+Fore.CYAN+"║")
+            
+    for inferiorL in range(117):
+        if(inferiorL == 0):
+            print(Cursor.POS(3,28)+Fore.CYAN+"╚", end="")
+        elif(inferiorL == 116):
+            print(Cursor.POS(118,28)+Fore.CYAN+"╝")
+        elif(inferiorL == 39):
+            print(Cursor.POS(42, 28)+Fore.CYAN+"╩")
+        else:
+            print(Cursor.POS(3+inferiorL,28)+Fore.CYAN+"═", end="")
+                        
+    for rightL in range(16):
+        print(Cursor.POS(118,12+rightL)+Fore.CYAN+"║")
+
 def frame(color = Fore.GREEN):
     for superiorL in range(120):
         if(superiorL == 0):
@@ -97,28 +144,96 @@ def frame(color = Fore.GREEN):
     for rightL in range(27):
         print(Cursor.POS(120,2+rightL)+color+"║")
     
-    menuFrame(color)
-    
 def newFile(name):
     try:
-        newFile = open(name, mode = "x")
-        print(Cursor.POS(46, 13)+Fore.GREEN+'Archivo creado exitosamente...')
-        print(Cursor.POS(46, 15)+Fore.GREEN+'Presione una tecla para ir al menu')
+        if(name.endswith(".txt")):
+            newFile = open(name, mode = "x")
+        else:
+            name = name + ".txt"
+            newFile = open(name, mode = "x")
+        newFile.close()
+        print(Cursor.POS(46, 14)+Fore.GREEN+'Archivo creado exitosamente...')
+        print(Cursor.POS(46, 15)+Fore.RED+'Presione una tecla para regresar')
         getch()
         menu()
     except FileExistsError:
-        print(Cursor.POS(46, 13)+Fore.GREEN+'Error: El archivo ya existe... \n')
-        print(Cursor.POS(46, 15)+Fore.GREEN+'Presione una tecla para ir al menu')
+        print(Cursor.POS(46, 14)+Fore.RED+'Error: El archivo ya existe...')
+        print(Cursor.POS(46, 15)+Fore.RED+'Presione una tecla para regresar')
         getch()
         menu()
+
+def editFile(fileName, txtFiles):
+    print(Cursor.POS(43,12)+Fore.CYAN+"Seguro que desea editar el archivo '"+fileName+"'?")
+    print(Cursor.POS(43,13)+Fore.CYAN+"Presione ENTER para aceptar o ESC para regresar a la seleccion...")
+    
+    opc = ord(getch())
+    if(opc == 27):
+        os.system('cls')
+        frame(Fore.CYAN)
+        editFrame()
+        editSign()
+        selectFile(txtFiles)
+    elif(opc == 13):
+        os.system('cls')
+        frame(Fore.CYAN)
+        editFrame()
+        editSign()
+        print(Cursor.POS(43,10)+Fore.CYAN+"[ENTER] Guardar y salir")
+        print(Cursor.POS(43,12)+Fore.CYAN+"Escriba el texto que desea agregar al archivo "+fileName)
+        subprocess.run('', shell = True)
+        print(Cursor.POS(43,13)+Fore.CYAN+"-> \0337")
+        newInfo = str(input('\0338'))
+        
+        thisFile = open(fileName, mode="a", encoding="utf-8")
+        thisFile.write(newInfo+" ")
+        thisFile.close()
+        
+        os.system('cls')
+        frame(Fore.CYAN)
+        editFrame()
+        editSign()
+        print(Cursor.POS(43,12)+Fore.CYAN+"El archivo '"+fileName+"' ha sido editado correctamente")
+        print(Cursor.POS(43,13)+Fore.RED+"Presione ENTER para editar un nuevo archivo o ESC para regresar al menu")
+        opc = ord(getch())
+        
+        if(opc == 13):
+            os.system('cls')
+            frame(Fore.CYAN)
+            editFrame()
+            editSign()
+            print(Cursor.POS(43,10)+Fore.CYAN+"[ESC] Salir al menu")
+            selectFile(txtFiles)
+        elif(opc == 27):
+            menu()
+
+def selectFile(txtFiles,selected = 0,fore = Fore.CYAN, back = Back.BLACK):
+
+    if(len(txtFiles) == 0):
+        print(Cursor.POS(5,4)+Fore.CYAN+"No hay archivos para mostrar")
+        print(Cursor.POS(5,5)+Fore.CYAN+"Primero debe crear un archivo")
+    else:
+        cont = 0
+        tab = 0
+        for t in txtFiles:
+            cont += 1
+            if(cont-1 == selected):
+                print(Cursor.POS(5+tab,3+cont)+fore+Back.WHITE+t)
+            else:
+                print(Cursor.POS(5+tab,3+cont)+Fore.CYAN+Back.BLACK+t)
+            if(cont == 14):
+                cont = 0
+                tab += 25
         
 def menu():
     os.system('cls')
     frame()
+    menuFrame()
     printInfo()
     menuSign()
     menuInfo()
+    
     step = 1
+    
     while(True):
         opc = ord(getch())
         if(opc == 80):
@@ -137,13 +252,49 @@ def menu():
         if(opc == 13) & (step == 1):
             os.system('cls')
             frame(Fore.RED)
+            menuFrame(Fore.RED)
             newFileSign()
             print(Cursor.POS(46, 11)+Fore.GREEN+'Ingrese el nombre de su archivo: ')
             subprocess.run('', shell = True)
             print(Cursor.POS(46, 12)+Fore.GREEN+'-> \0337')
-            name = input('\0338')
+            name = str(input('\0338'))
             
             newFile(name)
+                        
+        if(opc == 13) & (step == 2):
+            os.system('cls')
+            frame(Fore.CYAN)
+            editFrame()
+            editSign()
+            print(Cursor.POS(43,10)+Fore.CYAN+"[ESC] Salir al menu")
+            
+            files = os.listdir('.')
+            txtFiles = []
+            for f in files:
+                if f.endswith('.txt'):
+                    txtFiles.append(f)
+            
+            step = 0
+            selectFile(txtFiles,step)
+            
+            while(True):
+                opc = ord(getch())
+                if(opc == 80):
+                    if(step == len(txtFiles)-1):
+                        step = 0
+                    else:
+                        step += 1
+                    selectFile(txtFiles,step)
+                elif(opc == 72):
+                    if(step == 0):
+                        step = len(txtFiles)-1   
+                    else:
+                        step -= 1
+                    selectFile(txtFiles,step)
+                elif(opc == 13):
+                    editFile(txtFiles[step], txtFiles)
+                elif(opc == 27):
+                    menu()
         if(opc == 13) & (step == 3):
             os.system('cls')
             print("Ha salido del programa...")
--- pruebas.py
+++ pruebas.py
@@ -1,56 +1,17 @@
-# from msvcrt import getch
+from msvcrt import getch
 import os
-# dir = ord(getch())
-# print(dir)
-# if dir == 72:
-#     print("arriba")
-# elif dir == 75:
-#     print("izq")
-# elif dir == 77:
-#     print("der")
-# elif dir == 80:
-#     print("abajo")
+
+# arr = os.listdir('.')
+# txtarr = []
+
+# for j in arr:
+#     if j.endswith('.txt'):
+
+#         txtarr.append(j)
+
+# print(txtarr)
 
 
-def dateDict():
-    os.system('cls')
-    monthDict = {
-        'enero' : [1, 31],
-        'febrero' : [2, 28],
-        'marzo' : [3, 31],
-        'abril' : [4, 30],
-        'mayo' : [5, 31],
-        'junio' : [6, 30],
-        'julio' : [7, 31],
-        'agosto' : [8, 31],
-        'septiembre' : [9, 30],
-        'octubre' : [10, 31],
-        'noviembre' : [11, 30],
-        'diciembre' : [12, 31]
-    }
-    leapMonthDict = {
-        'enero' : [1, 31],
-        'febrero' : [2, 29],
-        'marzo' : [3, 31],
-        'abril' : [4, 30],
-        'mayo' : [5, 31],
-        'junio' : [6, 30],
-        'julio' : [7, 31],
-        'agosto' : [8, 31],
-        'septiembre' : [9, 30],
-        'octubre' : [10, 31],
-        'noviembre' : [11, 30],
-        'diciembre' : [12, 31]
-    }
-    yearDict = {}
-    year = 0
-    for i in range(539):
-        year = str(i + 1582)
-        yearDict[year] = monthDict
-        if(int(year) % 4 == 0) | (int(year) % 400 == 0):
-            yearDict[year] = leapMonthDict
-
-    print( yearDict["2019"]['febrero'])
-    
-    
-dateDict()
+# while(True):
+#     d = ord(getch())
+#     print(d)
